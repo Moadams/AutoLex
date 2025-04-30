7f52f5c1f9b5a314ca0c28771c697dc0e9d78a9b
Michael Okyere Adams <adamsmichael46@gmail.com>
2025-04-30 10:41:41 +0000

    add regular expression processor

---
+++ src/main/java/controller/RegexProcessor.java
@@ -0,0 +1,52 @@
+package main.java.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class RegexProcessor{
+    /**
+     * Finds all matches of a given regex pattern in the input text
+     *
+     * @param text the input text to search in
+     * @param regex the regex pattern to search for
+     * @return a list of strings containing all matches of the regex pattern in the input text
+     */
+    public List<String> findMatches(String text, String regex){
+        ArrayList<String> matches = new ArrayList<>();
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(text);
+        
+        if(matcher.find()){
+            matches.add(matcher.group());
+        }
+        return matches;
+    }
+
+    /**
+     * Replaces all matches of a given regex pattern in the input text with a given replacement
+     *
+     * @param inputText the input text to replace matches in
+     * @param regex the regex pattern to search for
+     * @param replacement the replacement for the matches
+     * @return the modified input text with all matches replaced
+     */
+    public String replaceMatches(String inputText, String regex, String replacement){
+        Pattern pattern = Pattern.compile(regex);
+        return pattern.matcher(inputText).replaceAll(replacement);
+    }
+
+    /**
+     * Finds out if a given regex pattern matches any substring in the input text
+     *
+     * @param text the input text to search in
+     * @param regex the regex pattern to search for
+     * @return true if a match is found, false otherwise
+     */
+    public boolean isMatchFound(String text, String regex){
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(text);
+        return matcher.find();
+    }
+}
