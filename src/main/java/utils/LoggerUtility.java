eae6730c7c5051bc5d77ed59e19f8cb451651043
Michael Okyere Adams <adamsmichael46@gmail.com>
2025-05-01 14:06:48 +0000

    update log method to use file processor controller

--- src/main/java/utils/LoggerUtility.java
+++ src/main/java/utils/LoggerUtility.java
@@ -1,32 +1,39 @@
 package main.java.utils;
 
-import java.io.FileWriter;
 import java.io.IOException;
-import java.io.PrintWriter;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
+import main.java.controller.FileProcessor;
+
 public class LoggerUtility {
     private static final String LOG_FILE = "logs/log.txt";
+    private final FileProcessor fileProcessor;
 
-    public static void log(String level, String message) {
-        try(PrintWriter writer = new PrintWriter(new FileWriter(LOG_FILE, true));) {
+    public LoggerUtility(FileProcessor fileProcessor) {
+        this.fileProcessor = fileProcessor;
+    }
+
+    public void log(String level, String message) {
+        try{
             String timeStamp = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
-            writer.printf("%s: %s %s\n", timeStamp, message);
+            String content = level + "" + timeStamp + ": " + message + "\n";
+            fileProcessor.appendToFile(LOG_FILE, content);
+            
         } catch (IOException e) {
             System.err.println("Logging failed: " + e.getMessage());
         }
     }
 
-    public static void logInfo(String message) {
+    public void logInfo(String message) {
         log("INFO", message);
     }
 
-    public static void logError(String message, Throwable throwable) {
+    public void logError(String message, Throwable throwable) {
         log("ERROR", message + " | Exception: " + throwable.getMessage());
     }
 
-    public static void logWarning(String message) {
+    public void logWarning(String message) {
         log("WARNING", message);
     }
 }
